962f051451247d0bb377f814165411603c0b617c
Patrick Kratschmer <Patrick.Kraetschmer@gmail.com>
2018-02-01 16:53:15 +0000

    update scripts - config

--- bioc_pipeline.sh
+++ bioc_pipeline.sh
@@ -1,40 +1,35 @@
 #!/usr/bin/env bash
 
-mkdir /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/aligned
+## Configuration
+# Programme executables
+CPUS=16
+STAR="/array/Patrick_RNA_seq/software/STAR-2.5.3a/bin/Linux_x86_64/STAR"
+SAMTOOLS="/array/Patrick_RNA_seq/software/samtools-1.6/samtools"
+GENOME="/array/Patrick_RNA_seq/reference/ref_genome/STAR/STAR_index"
 
-files=`cat fastq_files`
+# In- and output directories
+BASEDIR="/array/Patrick_RNA_seq/data/GEPD_run/bioconductor"
+FASTQ="$BASEDIR/FASTQ"
+mkdir $BASEDIR/aligned
+mkdir $BASEDIR/BAM
 
-for f in $files; do
-  /array/Patrick_RNA_seq/software/STAR-2.5.3a/bin/Linux_x86_64/STAR \
-  --runThreadN 4 \
-  --alignIntronMax 50000 \
-  --genomeDir /array/Patrick_RNA_seq/reference/ref_genome/STAR/STAR_index \
-  --readFilesIn /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/FASTQ/$f/$f\_R1_001.fastq \
-  /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/FASTQ/$f/$f\_R2_001.fastq \
-  --outFileNamePrefix /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/aligned/$f.
+
+## Main
+# 1. Align - max-intronlen is set to the 99th centile of Dmel intron lengths
+for f in $(cat fastq_files); do
+  $STAR --runThreadN $CPUS --alignIntronMax 26745 --genomeDir $GENOME \
+  --readFilesIn $FASTQ/$f/$f\_R1_001.fastq \
+  $FASTQ/$f/$f\_R2_001.fastq --outFileNamePrefix $BASEDIR/aligned/$f.
 done
 
-mkdir /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/BAM
-
-cd /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/aligned
-
-sams=`echo *.sam`
-
-for s in $sams; do
-  /array/Patrick_RNA_seq/software/samtools-1.6/samtools \
-  sort -@ 4 \
-  -o /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/BAM/$s.bam \
-  /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/aligned/$s
+cd $BASEDIR/aligned
+for s in $(echo *.sam); do
+  $SAMTOOLS sort -@ $CPUS -o $BASEDIR/BAM/$s.bam $BASEDIR/aligned/$s
 done
 
-cd /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/BAM
-
-bams=`echo *.bam`
-
-for b in $bams; do
-  /array/Patrick_RNA_seq/software/samtools-1.6/samtools \
-  index -@ 4 \
-  /array/Patrick_RNA_seq/data/GEPD_run/bioconductor/BAM/$b
+cd $BASEDIR/BAM
+for b in $(echo *.bam); do
+  $SAMTOOLS index -@ $CPUS $BASEDIR/BAM/$b
 done
 
 exit
--- tux_pipeline.sh
+++ tux_pipeline.sh
@@ -1,69 +1,74 @@
 #!/usr/bin/env bash
 
-mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/aligned
-files=`cat fastq_files`
-for f in $files; do
-  /array/Patrick_RNA_seq/software/hisat2-2.1.0/hisat2 \
-  -p 8 --dta \
-  -x /array/Patrick_RNA_seq/reference/ref_genome/bdgp6_tran/genome_tran \
-  -1 /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/FASTQ/$f/$f\_R1_001.fastq \
-  -2 /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/FASTQ/$f/$f\_R2_001.fastq \
-  -S /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/aligned/$f.sam \
+## Configuration
+# Programme executables
+CPUS=16
+HISAT2="/array/Patrick_RNA_seq/software/hisat2-2.1.0/hisat2"
+SAMTOOLS="/array/Patrick_RNA_seq/software/samtools-1.6/samtools"
+STRINGTIE="/array/Patrick_RNA_seq/software/stringtie-1.3.3b.Linux_x86_64/stringtie"
+GENOME="/array/Patrick_RNA_seq/reference/ref_genome/bdgp6_tran/genome_tran"
+GTF="/array/Patrick_RNA_seq/reference/gtf/Drosophila_melanogaster.BDGP6.91.gtf"
+
+# In- and output directories
+BASEDIR="/array/Patrick_RNA_seq/data/GEPD_run/tuxedo"
+FASTQ="$BASEDIR/FASTQ"
+mkdir $BASEDIR/aligned
+mkdir $BASEDIR/BAM
+mkdir $BASEDIR/assembled
+
+## Main
+# 1. Align - max-intronlen is set to the 99th centile of Dmel intron lengths
+for f in $(cat fastq_files); do
+  $HISAT2 -p $CPUS --dta -x $GENOME -1 $FASTQ/$f/$f\_R1_001.fastq \
+  -2 $FASTQ/$f/$f\_R2_001.fastq -S $BASEDIR/aligned/$f.sam \
   --rna-strandness RF --max-intronlen 26745
 done
 
-mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM
-cd /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/aligned
-sams=`echo *.sam`
-for s in $sams; do
-  /array/Patrick_RNA_seq/software/samtools-1.6/samtools \
-  sort -@ 4 \
-  -o /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM/$s.bam \
-  /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/aligned/$s
+# 2. Convert to BAM
+cd $BASEDIR/aligned
+for s in $(echo *.sam); do
+  $SAMTOOLS sort -@ $CPUS -o $BASEDIR/BAM/$s.bam \
+  $BASEDIR/aligned/$s
 done
 
-cd /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM
-bams=`echo *.bam`
-for b in $bams; do
-  /array/Patrick_RNA_seq/software/samtools-1.6/samtools \
-  index -@ 4 \
-  /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM/$b
+# 3. Index BAM
+cd $BASEDIR/BAM
+# for b in $(echo *.bam); do
+  # $SAMTOOLS index -@ $CPUS $BASEDIR/BAM/$b
+# done
+
+# 4. Assemble and quantify
+for b in $(echo *.bam); do
+  $STRINGTIE -p $CPUS -G $GTF -o $BASEDIR/assembled/$b.gtf \
+  $BASEDIR/BAM/$b
 done
 
-mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/assembled
-for b in $bams; do
-  /array/Patrick_RNA_seq/software/stringtie-1.3.3b.Linux_x86_64/stringtie \
-  -p 8 \
-  -G /array/Patrick_RNA_seq/reference/gtf/Drosophila_melanogaster.BDGP6.91.gtf \
-  -o /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/assembled/$b.gtf \
-  -l $b\_assembled \
-  /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM/$b
-done
 
-cd /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/assembled
-gtf_dirs=ls
-touch mergelist.txt
-for gtf_dir in $gtf_dirs; do
-  cd gtf_dir
-  printf '%s\n' *.gtf > ../mergelist.txt
-  cd ..
-done
-mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/merged_gtf
-/array/Patrick_RNA_seq/software/stringtie-1.3.3b.Linux_x86_64/stringtie \
--p 4 \
--G /array/Patrick_RNA_seq/reference/gtf/Drosophila_melanogaster.BDGP6.91.gtf \
--o /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/merged_gtf/merged.gtf \
-/array/Patrick_RNA_seq/data/GEPD_run/tuxedo/assembled/mergelist.txt
 
-cd /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM
-mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/ballgown
-for b in $bams; do
-  mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/ballgown/$b
-  /array/Patrick_RNA_seq/software/stringtie-1.3.3b.Linux_x86_64/stringtie \
-  -e -B -p 8 \
-  -G /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/merged_gtf/merged.gtf \
-  -o /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/ballgown/$b/$b\_merged.gtf \
-  /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM/$b
-done
+
+
+
+
+
+# cd /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/assembled
+# touch mergelist.txt
+# for gtf_dir in $(ls); do
+# mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/merged_gtf
+# /array/Patrick_RNA_seq/software/stringtie-1.3.3b.Linux_x86_64/stringtie \
+# -p 4 \
+# -G /array/Patrick_RNA_seq/reference/gtf/Drosophila_melanogaster.BDGP6.91.gtf \
+# -o /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/merged_gtf/merged.gtf \
+# /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/assembled/mergelist.txt
+
+# cd /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM
+# mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/ballgown
+# for b in $(echo *.bam); do
+  # mkdir /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/ballgown/$b
+  # /array/Patrick_RNA_seq/software/stringtie-1.3.3b.Linux_x86_64/stringtie \
+  # -e -B -p 8 \
+  # -G /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/merged_gtf/merged.gtf \
+  # -o /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/ballgown/$b/$b\_merged.gtf \
+  # /array/Patrick_RNA_seq/data/GEPD_run/tuxedo/BAM/$b
+# done
 
 exit
